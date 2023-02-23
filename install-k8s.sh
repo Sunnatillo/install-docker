3d7d3d461b9f38112726b950633c5f3769cf52f1
Sunnatillo <sunnat.samadov@est.tech>
2023-02-23 14:35:15 +0200

    add containerd config removal

--- install-k8s.sh
+++ install-k8s.sh
@@ -12,3 +12,9 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://a
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo rm /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+#sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.201.10.158
