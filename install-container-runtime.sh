2322ed30179bd9176be26ca9e2288d079a287f99
Sunnatillo <sunnat.samadov@est.tech>
2023-02-23 16:11:13 +0200

    Update installation scripts  - remove ansible playbook  - add containerd related configuration

---
+++ install-container-runtime.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -xe
+
+# Configure required modules
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Configure required sysctl to persist across system reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+#Apply sysctl parameters without rebooting to current running environment
+sudo sysctl --system
+
+# Install containerd packages
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update 
+sudo apt-get install -y containerd.io
+
+
+# Create a containerd configuration file
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+# Set the cgroup driver for runc to systemd
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
