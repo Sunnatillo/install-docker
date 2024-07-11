218a170a9766c056761c4073adfe3ca6100258fe
Sunnatillo <sunnat.samadov@est.tech>
2024-07-11 15:41:01 +0300

    set ip_forward to 1
    Signed-off-by: Sunnatillo <sunnat.samadov@est.tech>

--- install-container-runtime.sh
+++ install-container-runtime.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -xe
 
+# Configure required sysctl to persist across system reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
 # Configure required modules
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
@@ -9,13 +16,6 @@ EOF
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
-# Configure required sysctl to persist across system reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
 
 #Apply sysctl parameters without rebooting to current running environment
 sudo sysctl --system
@@ -37,3 +37,6 @@ sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 # Set the cgroup driver for runc to systemd
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo sh -c "echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf"
